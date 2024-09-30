aef9ce7ec46e00cce772085ce50446d08c25aaa0
Tony Vinicius Cavalcante <82682652+tonyviniciuscavalcante@users.noreply.github.com>
2024-09-30 11:47:31 -0300

    Update Jogo.kt

--- AluGames/src/main/kotlin/Jogo.kt
+++ AluGames/src/main/kotlin/Jogo.kt
@@ -2,4 +2,11 @@ class Jogo {
     var titulo = ""
     var capa = ""
     val descricao = ""
+
+    override fun toString(): String {
+        return "Meu jogo: \n" +
+                "Titulo: $titulo \n" +
+                "Capa: $capa \n" +
+                "Descricao: $descricao"
+    }
 }
